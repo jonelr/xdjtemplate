51fbea0fda2285fc7352baefdf44ec9e799a6be3
Jonel R <jonel@road14.com>
2018-01-18 18:30:41 -0600

    Initial

---
+++ xdjtemplate/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for xdjtemplate project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "xdjtemplate.settings")
+
+application = get_wsgi_application()
